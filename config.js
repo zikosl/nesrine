7c8be007c8e68986e40e1a94c7c9f357aa8b684f
zikosl <z.slimi@finnetude.com>
2023-11-06 22:25:19 +0100

    final commit

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+
+    texts: [
+        "Little Imane Lbakay",
+        "Dazzling little Girl",
+        "Strong Independant Baby Girl",
+        "Prettiest Girl ever",
+        "To the girl who brings beauty to the world",
+        "A new year of your life has begun",
+        "Thats a little Gift For u",
+        "May this special day be filled with colorful inspiration",
+        "pixel-perfect dreams",
+        "and all the tools you need to design the world of your imagination",
+        "Keep sketching your dreams",
+        "may your future be as user-friendly and beautifully designed as your heart desires",
+        "dont think bezaf it's just 22 😆",
+        "🎂🎂🎂",
+    ],
+
+    imgs: {
+        "imane": "./imgs/xiaokeai.jpg",
+    },
+    desc: {
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "Somthing Missing",
+        cake_fadein: "Cake？",
+        light_candle: "Light？",
+        wish_message: "Happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
